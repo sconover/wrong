0ba716796cd41012ddc483a294f0a22bf713a012
Alex Chaffee <alex@stinky.com>
2010-08-21 10:35:07 -0700

    change checked-in predicated to a git submodule

--- lib/predicated.rb
+++
@@ -1,6 +0,0 @@
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-
-require "predicated/gem_check"
-require "predicated/predicate"
-require "predicated/evaluate"
-require "predicated/constrain"
--- lib/wrong.rb
+++ lib/wrong.rb
@@ -1,5 +1,5 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
-require "predicated"
+require "predicated/lib/predicated"
 
 require "wrong/assert"
 require "wrong/chunk"
