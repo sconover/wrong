ce9b8e29023032b79fd2f7dc9806e94fc1eb3d05
Akira Matsuda <ronnie@dio.jp>
2014-07-24 02:30:12 +0900

    "warning: assigned but unused variable - success"

--- lib/wrong/eventually.rb
+++ lib/wrong/eventually.rb
@@ -7,7 +7,6 @@ module Wrong
       timeout = options[:timeout] || 5
       delay = options[:delay] || 0.25
       last_error = nil
-      success = nil
       begin_time = Time.now      
       while (Time.now - begin_time) < timeout
         begin
